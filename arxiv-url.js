4669d95aa37f35c232952ca331a67190ed69ac37
Jakukyo Friel <weakish@gmail.com>
2018-03-02 22:33:22 +0800

    It replaces arxiv pdf links on web pages to arxiv abstract link.

---
+++ arxiv-url.js
@@ -0,0 +1,11 @@
+let arxiv_pdf_pattern = /http(s)?:\/\/[www.]?arxiv.org\/pdf\//;
+let anchors = document.getElementsByTagName('a');
+for (let anchor of anchors) {
+    let href_ = anchor.href;
+    if (href_.search(arxiv_pdf_pattern) != -1) {
+        href_ = href_.replace(".pdf", "");
+        href_ = href_.replace("arxiv.org/pdf/", "arxiv.org/abs/");
+        anchor.href = href_;
+    }
+}
+
