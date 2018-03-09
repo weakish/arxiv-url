075496d00e5b506847c1cf5a9e3b65edc17f4666
Jakukyo Friel <weakish@gmail.com>
2018-03-10 00:07:08 +0800

    Avoid replace pdf urls on arxiv.org.

--- arxiv-url.js
+++ arxiv-url.js
@@ -1,11 +1,14 @@
-let arxiv_pdf_pattern = /http(s)?:\/\/[www.]?arxiv.org\/pdf\//;
-let anchors = document.getElementsByTagName('a');
-for (let anchor of anchors) {
-    let href_ = anchor.href;
-    if (href_.search(arxiv_pdf_pattern) != -1) {
-        href_ = href_.replace(".pdf", "");
-        href_ = href_.replace("arxiv.org/pdf/", "arxiv.org/abs/");
-        anchor.href = href_;
+let current_domain = window.location.hostname;
+if (current_domain !== "arxiv.org") {
+    let arxiv_pdf_pattern = /http(s)?:\/\/[www.]?arxiv.org\/pdf\//;
+    let anchors = document.getElementsByTagName('a');
+    for (let anchor of anchors) {
+        let href_ = anchor.href;
+        if (href_.search(arxiv_pdf_pattern) != -1) {
+            href_ = href_.replace(".pdf", "");
+            href_ = href_.replace("arxiv.org/pdf/", "arxiv.org/abs/");
+            anchor.href = href_;
+        }
     }
 }
 
